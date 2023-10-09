e254b1836a125d45f4eb82b621da0fa988aaa3b2
Kaike Nascimento <kaike.nascimento@zenvia.com>
2023-10-09 20:34:37 -0300

    [todolist] Creating TODOList project

---
+++ src/main/java/com/kaike/study/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package com.kaike.study.todolist.user;
+
+public class UserModel {
+
+    public String username;
+    public String name;
+    public String password;
+    public String mail;
+    
+}
