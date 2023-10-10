e6e1cddcf2228c5bf407df5d77f5e6c1776e5f21
Kaike Nascimento <kaike_soares123@hotmail.com>
2023-10-10 19:04:41 -0300

    [TODOList] Adding connection with DB, creating Repository and returning HTTP Status Code

---
+++ src/main/java/com/kaike/study/todolist/user/JpaRepository.java
@@ -0,0 +1,5 @@
+package com.kaike.study.todolist.user;
+
+public interface JpaRepository<T1, T2> {
+
+}
--- src/main/java/com/kaike/study/todolist/user/UserModel.java
+++ src/main/java/com/kaike/study/todolist/user/UserModel.java
@@ -1,10 +1,30 @@
 package com.kaike.study.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data // Getters e Setters do Lombok
+@Entity(name = "tb_users")
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true) // Define que não será possível criar users duplicados
     public String username;
     public String name;
     public String password;
     public String mail;
-    
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
